373fc75af5483328fb79d54da59fd4842a3abfd4
0.0 <15982984+zero-00-zero@user.noreply.gitee.com>
2025-07-19 14:18:59 +0800

    新信息的数量更新，注销功能完善

--- mainPackage/pages/systemManage/systemManage.js
+++ mainPackage/pages/systemManage/systemManage.js
@@ -5,6 +5,42 @@ Page({
     phone:11111111111,
     newMessageAmount:3
   },
+  onLoad() {
+      console.log('systemManage');
+    this.fetchNewMsg();
+  },
+  fetchNewMsg() {
+    wx.request({
+      url: `${getApp().globalData.serverUrl}/diServer/system/notice/myMsgList`,
+      method: 'GET',
+      header: {
+        'Authorization': `Bearer ${getApp().globalData.token}`
+      },
+      success: (res) => {
+        if (res.statusCode === 200 && res.data.code === 200) {
+            const data = res.data.data || []; 
+            this.setData({
+                newMessageAmount: data.length
+          });
+          console.log('NewMessage:',res.data.data);
+        } else {
+          // 请求失败，使用本地默认数据
+          this.setData({ 
+            errorMsg: res.data.message || '获取数据失败，使用默认数据',
+          });
+          console.log('errorMsg');
+        }
+      },
+      fail: (err) => {
+        // 请求失败，使用本地默认数据
+        this.setData({ 
+          errorMsg: '网络请求失败，使用默认数据',
+        });
+        console.error(err);
+      },
+    });
+  },
+
   goToPersonalInformation(){
     wx.navigateTo({
       url: '/mainPackage/pages/personalInformation/personalInformation',
@@ -29,6 +65,7 @@ Page({
     const app = getApp();
     app.globalData.userInfo = null;
     app.globalData.authenticated = false;
+    app.token = null;
     wx.redirectTo({
       url: '/pages/login/login',
     })
