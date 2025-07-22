86304618763fbd8c5fee0aea031df19280dd1be4
0.0 <15982984+zero-00-zero@user.noreply.gitee.com>
2025-07-22 21:44:07 +0800

    商家部分完成

--- merchantPackage/pages/merchants/merchants.js
+++ merchantPackage/pages/merchants/merchants.js
@@ -58,6 +58,7 @@ Page({
   onLoad(){
     this.fetchContacts();
   },
+  //数据更新
   fetchContacts(){
     wx.request({
         url: `${getApp().globalData.serverUrl}/diServer/company/list?pageNum=1&pageSize=10`,
@@ -89,6 +90,7 @@ Page({
         },
       });
   },
+  //搜索
   inputMerchant(){
     if(this.data.keyword==''){
       this.setData({
@@ -100,18 +102,54 @@ Page({
       })
     }
   },
-  confirmDelete(){
-    wx.showModal({
-      title: '确认',
-      content: '确定要删除吗？',
-      success (res) {
-          if (res.confirm) {
-              console.log('用户点击确定')
-          } else if (res.cancel) {
-              console.log('用户点击取消')
-          }
-      }
-  });
+  confirmDelete(e){
+    const id = e.currentTarget.dataset.id || '';
+    if(id != '')
+    {
+        wx.showModal({
+            title: '确认',
+            content: '确定要删除吗？',
+            success (res) {
+                if (res.confirm) {
+                  wx.request({
+                      url: `${getApp().globalData.serverUrl}/diServer/company/${id}`,
+                      method: 'DELETE',
+                      header: {
+                        'Authorization': `Bearer ${getApp().globalData.token}`
+                      },
+                      success: (res) => {
+                        if (res.statusCode === 200 && res.data.code === 200) {
+                            // 刷新当前页面数据
+                            setTimeout(() => {
+                            const currentPage = getCurrentPages()[getCurrentPages().length - 1];
+                            currentPage.onLoad();
+                            }, 500);
+                        } else {
+                          // 请求失败，使用本地默认数据
+                          console.log('errorMsg');
+                        }
+                      },
+                      fail: (err) => {
+                        this.setData({ 
+                          errorMsg: '网络请求失败，使用默认数据',
+                        });
+                        console.error(err);
+                      },
+                    });
+                } else if (res.cancel) {
+                    console.log('用户点击取消')
+                }
+            }
+        });
+    }
+    else{
+        wx.showToast({
+            title: '未知错误',
+            icon: 'none',
+            duration: 2000
+          });
+    }
+
   },
   goToContacts(){
     wx.redirectTo({
